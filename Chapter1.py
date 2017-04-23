5840627288ef0fe49d0008779e9393733bff1263
Kai Lee <hlee027@gmail.com>
2017-04-23 11:54:31 -0500

    Review for chapter 1

--- Chapter1.py
+++ Chapter1.py
@@ -1,22 +1,19 @@
 # # 1.1
-#20 minutes, I realized this is still cheating afterwards
-def unique_char(string):
-    c1 = [0]
-    empty = False
-    string = list(string)
-    while len(string) != 0:
-        if string[-1] not in string[:-1]: string.pop()
-        else: return False
-    return True
-
 # I re-did this after 1.3, finished in 5 minutes
 def unique_char2(string):
-    c = [0]*256
-    for i in range(len(string)):
-        c[ord(string[i]) - ord('\x00')] += 1
-    for i in range(len(c)):
-        if c[i] > 1: return False
-    return True
+  ascii_characters = 256
+  cs = [0] * ascii_characters
+  for c in string:
+      cs[ord(c) - ord('\x00')] += 1
+  for i in range(ascii_characters):
+      if cs[i] > 1: return False
+  return True
+
+  d = {}
+  for c in string:
+    if c in d: return False
+    else: d[c] = 1
+  return True
 
 print(unique_char2('blah'))
 print(unique_char2('hello'))
@@ -24,20 +21,20 @@ print(unique_char2('hello'))
 
 # # 1.3
 # I learned this one before, took me 8 minutes
-
 def is_permutation(string1, string2):
     if len(string1) != len(string2): return False 
     
-    c1 = [0]*256
-    c2 = [0]*256
+    c1 = [0] * 256
+    c2 = [0] * 256
     
-    for i in range(0,len(string1)):
-        c1[ord(string1[i]) - ord('\x00')] += 1
-    
-    for i in range(0,len(string2)):
-        c2[ord(string2[i]) - ord('\x00')] += 1
+    for c in string1:
+        c1[ord(c) - ord('\x00')] += 1
+
+    for c in string2:
+        c2[ord(c) - ord('\x00')] += 1
     
     return c1 == c2
+  
 print(is_permutation('ab]c3', ']cba3'))
 print(is_permutation('adf', 'cba'))
 
@@ -45,76 +42,60 @@ print(is_permutation('adf', 'cba'))
 # # 1.4
 # Took me 51 minutes.  I can't think of how to do it without using insert and join since I cannot mute string.
 def space_replacement(string):
-    c = [0]*len(string)
-    for i in range(len(string)):
-        c[i] = ord(string[i])
-        if c[i] == 32: c[i] = 37
-    c_copy = c.copy()
-    count = 0
-    for i in range(len(c)):
-        if c[i] == 37: 
-            c_copy.insert(i + count*2 + 1, 50)
-            c_copy.insert(i + count*2 + 2, 48)
-            count += 1
-    char_array = []
-    for num in c_copy:
-        char_array.append(chr(num))
-    return ''.join(char_array)
+  new_string = ''
+  for c in string:
+    val = '%20' if c == ' ' else c
+    new_string += val
+  return new_string
+
+  return ''.join(['%20' if c == ' ' else c for c in string])
 
 print(space_replacement('Mr John Smith'))
-
 
 # # 1.5
 # 46 minutes
 
 def compress_string(string):
-    new_string = ''
-    for pos1 in range(len(string)):
-        if pos1 == 0 or string[pos1] != string[pos1-1]:
-            count = 1
-            pos2 = pos1
-            while pos2 < len(string)-1 and string[pos2] == string[pos2+1]:
-                count += 1
-                pos2 += 1
-            new_string += string[pos1] + str(count)
-    if len(new_string) >= len(string):
-        return string
-    else:
-        return new_string
+  new_string = ''
+  for pos1 in range(len(string)):
+    if pos1 == 0 or string[pos1] != string[pos1-1]:
+      count = 1
+      pos2 = pos1
+      while pos2 < len(string)-1 and string[pos2] == string[pos2 + 1]:
+        count += 1
+        pos2 += 1
+      new_string += string[pos1] + str(count)
+  return string if len(new_string) >= len(string) else new_string
 
-print(compress_string('abc[['))
-print(compress_string('aabcccccaaa'))
+print(compress_string('abcs[[')) # -> abcs[[
+print(compress_string('aabcccccaaa')) # -> 'a2b1c5a3'
 
 # # 1.7
 # Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
 
 # Took me a little over 20 minutes
+def setZero(matrixrix):
+  matrix = [[1,  2,  3,  4,  5],
+         [6,  7,  8,  9,  10],
+         [11, 12, 13, 14, 15],
+         [16, 17, 18, 19, 20],
+         [21, 22, 23, 24, 0]]
+  zero_coord = []
+  for i in range(len(matrix)):
+      for j in range(len(matrix[i])):
+          if matrix[i][j] == 0:
+              zero_coord.append((i,j))
 
-mat = [[1,  2,  3,  4,  5],
-       [6,  7,  8,  9,  10],
-       [11, 12, 13, 14, 15],
-       [16, 17, 18, 19, 20],
-       [21, 22, 23, 24, 0]]
-
-zero_coord = []
-for i in range(len(mat)):
-    for j in range(len(mat[i])):
-        if mat[i][j] == 0:
-            zero_coord.append((i,j))
-
-for i,j in zero_coord:
-    mat[i] = 5*[0]
-    for k in range(len(mat)):
-        mat[k][j] = 0
+  for i, j in zero_coord:
+      matrix[i] = len(matrix[i]5) * [0]
+      for k in range(len(matrix)):
+          matrix[k][j] = 0
     
 print(zero_coord)
-print(mat)
+print(matrix)
 
 
 # In[ ]:
-
-
-
 
 # # 1.8
 
@@ -130,9 +111,8 @@ def isSubstring(word1, word2):
     return word2 in word1
 
 def isRotation(word1, word2):
-    return isSubstring(word1+word1, word2)
+    return isSubstring(word1 + word1, word2)
 
 print(isSubstring('waterbottle','erbottlewat'))
-print(isRotation('waterbottle','erbottlewat'))
-
+print(isRotation('waterbottle','erbottlewat')) -> True
 
